5e0e6e5ef20e0ae35b07dae683b1bd945f286623
Frank Marcelo <frankitoy@gmail.com>
2020-01-23 00:20:01 +1100

    configure material design modules

--- apps/mono-repo/src/app/app.component.ts
+++ apps/mono-repo/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mr-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ apps/mono-repo/src/app/material/material.module.ts
@@ -0,0 +1,50 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  MatSidenavModule,
+  MatListModule,
+  MatTooltipModule,
+  MatOptionModule,
+  MatSelectModule,
+  MatMenuModule,
+  MatSnackBarModule,
+  MatGridListModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatRadioModule,
+  MatCheckboxModule,
+  MatCardModule,
+  MatProgressSpinnerModule,
+  MatRippleModule,
+  MatDialogModule,
+  MatSlideToggleModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTooltipModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatSnackBarModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatRippleModule,
+    MatDialogModule,
+    MatSlideToggleModule
+  ],
+  declarations: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class MaterialModule { }
